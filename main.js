ef2f8a27687e68c32f29e1aacd0a86770318421f
Nathan Martin <nathanmartin5937@gmail.com>
2021-02-03 20:45:21 -0600

    created for loop

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-console.log("CONNECTED");
-
-var projects = [
+var projects = objects [
     {
-    title: "Project 1", 
-    screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
-    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
-    available: true,
-    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
-  },
-  {
     title: "Cool Project", 
     screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
     description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
@@ -21,6 +10,15 @@ var projects = [
   },
   {
     title: "Cool Project", 
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+  },
+  {
+    title: "cool Project", 
     screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
     description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
     technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
@@ -30,3 +28,30 @@ var projects = [
   }
 ]
 
+const personalCards = (e) => {
+  let domString = "";
+  for (let i = 0; i < e.length; i++) {
+    if (projects[i].available == true) {
+      domString += `<div class="card" style="width: 18rem;" id=${i}>
+                    <div><h1 class="card-title">${e[i].title}</h1></div>
+                    <img src="${e[i].screenshot}" class="card-img">
+                    <div class="card-body">
+                      <p class="card-text">${e[i].description}</p>
+                      <p class="card-text">${e[i].technologiesUsed}</p>
+                      <a href="#" class="card-link">${e[i].Url}</a>
+                      <a href="#" class="card-link">${e[i].githubUrl}</a>
+                    </div>
+                  </div>`
+    }
+  };
+}
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = textToPrint;
+}
+
+const init = () => {
+  personalCards();
+};
+
+init();
